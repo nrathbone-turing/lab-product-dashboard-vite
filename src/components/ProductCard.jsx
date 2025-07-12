7e760213f053b0b1d02d55e197fec58b13a3a42e
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-12 10:16:11 -0600

    add conditional styling and remove button to ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from '../styles/ProductCard.module.css';
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product,onRemove }) => {
+  // Apply conditional class to <div> for out-of-stock items
+  const cardClass = product.inStock
+  ? styles.productCard
+  : `${styles.productCard} ${styles.outOfStockClass}`;
+
   return (
-    <div className>
-      {/* TODO: Apply conditional class to <div> above for out-of-stock items */}
-      
-      {/* TODO: Display product name */}
+    <div className={cardClass} data-testid={`product-card-${product.name.toLowerCase()}`}>
+      <h3>{product.name}</h3> {/* Display product name */}
+      <p>{product.price}</p> {/* Display product price */}
+      {/* Show if the product is in stock or out of stock */}
+      <p>{product.inStock ? 'In Stock' : 'Out of Stock'}</p>
+      {/* Add an image tag with placeholder image for products */}
+      <img
+        src={`https://via.placeholder.com/150?text=${product.name}`}
+        alt={product.name}
+      />
+      {/* Button that removes a product from the dashboard */}
+      <button onClick={() => onRemove(product.id)}>Remove</button>
 
-      {/* TODO: Display product price */}
-
-      {/* TODO: Show if the product is in stock or out of stock */}
-      
     </div>
   );
 };
